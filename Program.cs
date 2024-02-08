f427e88728409fb9c74638dc83820085f56d0bff
Ashlesh Shrivastava <ashlesh.shrivastava@wissen.com>
2024-02-08 13:22:13 +0530

    Added SSO Logic

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+//This method is going to ask for Microsoft EntraID for OIDC Authentication using Access Tokens and ID Tokens
+//using Microsoft.AspNetCore.Authentication.OpenIdConnect; the sso logic is wrapped within this package there is no aditional logic required for sso.
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+  .AddMicrosoftIdentityWebApp(builder.Configuration);
+
+builder.Services.AddControllersWithViews(options => 
+{
+    var policy = new AuthorizationPolicyBuilder()
+         .RequireAuthenticatedUser()
+         .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+      
+});
+
+// This method requires only Authenticted users to view the restricted URL
+builder.Services.AddRazorPages()
+    .AddMvcOptions(options => 
+    {
+        var policy = new AuthorizationPolicyBuilder()
+         .RequireAuthenticatedUser()
+         .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+
+
+    })
+    .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
@@ -18,6 +49,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+//This Function asks for OIDC Authentication before Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
